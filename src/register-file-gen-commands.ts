24246d5d0ee33f357b6a13ea42ab1c413206b187
wellwind <wellwind@mail2000.com.tw>
2019-12-08 19:31:27 +0800

    fix files output

--- src/register-file-gen-commands.ts
+++ src/register-file-gen-commands.ts
@@ -1,19 +1,19 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as vscode from 'vscode';
-import { camelize, dasherize } from './utils/string';
+import * as fs from "fs";
+import * as path from "path";
+import * as vscode from "vscode";
+import { camelize, dasherize } from "./utils/string";
 
 const generateSchematic = (collectionPath: string, name: string) => {
   const dirName = path.join(path.dirname(collectionPath), dasherize(name));
-  const indexFilePath = path.join(dirName, 'index.ts');
-  const schemaFilePath = path.join(dirName, 'schema.json');
+  const indexFilePath = path.join(dirName, "index.ts");
+  const schemaFilePath = path.join(dirName, "schema.json");
   if (!fs.existsSync(dirName)) {
     fs.mkdirSync(dirName);
   }
 
   const indexContent = `import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-export default function ${camelize(name)}(_options: any): Rule {
+export function ${camelize(name)}(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
 
     return tree;
@@ -40,7 +40,7 @@ export default function ${camelize(name)}(_options: any): Rule {
 };
 
 const updateCollectionJson = (path: string, name: string) => {
-  const collectionJson = JSON.parse(fs.readFileSync(path).toString('UTF-8'));
+  const collectionJson = JSON.parse(fs.readFileSync(path).toString("UTF-8"));
 
   collectionJson.schematics[name] = {
     description: name,
@@ -56,9 +56,11 @@ const isSchemaWorkspace = async () => {
   const foundPackageFile = fs.existsSync(packageJsonFilePath);
 
   if (foundPackageFile) {
-    const collectionFilePath = getCollectionFilePath() || '';
+    const collectionFilePath = getCollectionFilePath() || "";
     if (collectionFilePath) {
-      const collectionFilePhysicalPath = getPhysicalFilePath(collectionFilePath);
+      const collectionFilePhysicalPath = getPhysicalFilePath(
+        collectionFilePath
+      );
 
       const foundCollectionFile = fs.existsSync(collectionFilePhysicalPath);
       if (foundCollectionFile) {
@@ -70,36 +72,43 @@ const isSchemaWorkspace = async () => {
 };
 
 const getPhysicalFilePath = (file: string) => {
-  return path.join(vscode.workspace.rootPath || '', file || '');
+  return path.join(vscode.workspace.rootPath || "", file || "");
 };
 
 const getCollectionFilePath = () => {
   const packageJsonFilePath = getPackageJsonFilePath();
-  const packageJsonContent = JSON.parse(fs.readFileSync(packageJsonFilePath).toString('UTF-8'));
+  const packageJsonContent = JSON.parse(
+    fs.readFileSync(packageJsonFilePath).toString("UTF-8")
+  );
   const collectionFilePath = packageJsonContent.schematics;
   return collectionFilePath;
 };
 
 const getPackageJsonFilePath = () => {
   const workspaceRoot = vscode.workspace.rootPath;
-  const packageJsonFilePath = path.join(workspaceRoot || '', 'package.json');
+  const packageJsonFilePath = path.join(workspaceRoot || "", "package.json");
   return packageJsonFilePath;
 };
 
 export function registerFileGenCommands(context: vscode.ExtensionContext) {
-  const command = 'schematicToolkit.genSchematic';
+  const command = "schematicToolkit.genSchematic";
 
   const commandHandler = async () => {
     if (isSchemaWorkspace()) {
-      const collectionFilePath = getCollectionFilePath() || '';
-      const collectionFilePhysicalPath = getPhysicalFilePath(collectionFilePath);
+      const collectionFilePath = getCollectionFilePath() || "";
+      const collectionFilePhysicalPath = getPhysicalFilePath(
+        collectionFilePath
+      );
 
-      const collectionJson = JSON.parse(fs.readFileSync(collectionFilePhysicalPath).toString('UTF-8'));
-      const schemaName = (await vscode.window.showInputBox({ prompt: 'Schematic Name:' })) || '';
+      const collectionJson = JSON.parse(
+        fs.readFileSync(collectionFilePhysicalPath).toString("UTF-8")
+      );
+      const schemaName =
+        (await vscode.window.showInputBox({ prompt: "Schematic Name:" })) || "";
 
       if (schemaName) {
         if (collectionJson.schematics[dasherize(schemaName)]) {
-          vscode.window.showErrorMessage('This schematic name is exist.');
+          vscode.window.showErrorMessage("This schematic name is exist.");
           return;
         } else {
           updateCollectionJson(collectionFilePhysicalPath, schemaName);
@@ -107,9 +116,11 @@ export function registerFileGenCommands(context: vscode.ExtensionContext) {
         }
       }
     } else {
-      vscode.window.showErrorMessage('You are not in a schematic workspace.');
+      vscode.window.showErrorMessage("You are not in a schematic workspace.");
     }
   };
 
-  context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+  context.subscriptions.push(
+    vscode.commands.registerCommand(command, commandHandler)
+  );
 }
